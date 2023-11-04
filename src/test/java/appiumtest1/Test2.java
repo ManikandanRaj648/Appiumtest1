a5e196c2d520e1e5a807fb58452e0ad452b22d8c
manir <manir@LAPTOP-B5D0S9TR>
2023-11-04 19:50:17 +0530

    Commit to New branch

---
+++ src/test/java/appiumtest1/Test2.java
@@ -0,0 +1,18 @@
+package appiumtest1;
+
+public class Test2 {
+	int a=2;
+	public int add() {
+		int a=2;
+		int b=5;
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test2 tet=new Test2();
+		tet.add();
+
+	}
+
+}
